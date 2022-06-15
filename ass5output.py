6981d1b4129b5b069640b7c6306a09db7c6f40b1
Pratham-sharma07 <102985188+Pratham-sharma07@users.noreply.github.com>
2022-06-16 00:05:33 +0700

    Add files via upload

---
+++ ass5output.py
@@ -0,0 +1,99 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:/Users/acer/Desktop/Assignment_5 input.py =============
+Type your string here: hello
+olleh
+Enter lower range limit : 2
+Enter upper range limit : 10
+Enter the number to be divided by : 5
+5
+10
+Enter the side 1 : 5
+Enter the side 2 : 6
+Enter the side 3 : 7
+The combination exists
+The area of a triangle is:  14.7
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+The number of rows to be printed: 5
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+Enter the lowest range value : 4
+Enter the upper range value : 40
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+77
+154
+231
+308
+385
+462
+Enter the number: 40
+Enter the number: 50
+Enter the number: -60
+Enter the number: -30
+Enter the number: 80
+Enter the number: 70
+Enter the number: 90
+Enter the number: 60
+Enter the number: 68
+Enter the number: 55
+The negative numbers are: 
+-60
+-30
+The positive numbers are: 
+40
+50
+80
+70
+90
+60
+68
+55
+The odd numbers are: 
+55
+The even numbers are: 
+40
+50
+-60
+-30
+80
+70
+90
+60
+68
+Enter the number to be counted: 60
+It occurs  1  times
+Enter the word to be added in list: yo
+Enter the word to be added in list: i
+Enter the word to be added in list: am
+Enter the word to be added in list: so
+Enter the word to be added in list: and
+Enter the word to be added in list: so
+Enter the word to be added in list: beyond
+Enter the word to be added in list: words
+Enter the word to be added in list: or
+Enter the word to be added in list: is
+Enter the word to be counted: so
+so occurs 2 times.
